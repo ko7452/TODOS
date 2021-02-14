9174c35664d0d1686dc0afe210487c41a6750071
KoYunHyuk <sksgur3217@gmail.com>
2021-02-14 14:28:42 +0900

    complate stting in TodoItemList for TodoItem

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -4,7 +4,13 @@ class TodoItemList extends Component {
   render() {
     const { todos, onToggle, onRemove } = this.props;
 
-    return <div>{/* TODO */}</div>;
+    return (
+      <div>
+        <TodoItem text="안녕" />
+        <TodoItem text="리액트" />
+        <TodoItem text="반가워" />
+      </div>
+    );
   }
 }
 
