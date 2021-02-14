2640047181cf13d804157e25afb18eb39dbee0d3
KoYunHyuk <sksgur3217@gmail.com>
2021-02-14 15:39:41 +0900

    complaated refactoring in use shouldComponentUpdate

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -3,6 +3,10 @@ import React, { Component } from "react";
 import TodoItem from "./TodoItem";
 
 class TodoItemList extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.todos !== nextProps.todos;
+  }
+
   render() {
     const { todos, onToggle, onRemove } = this.props;
 
